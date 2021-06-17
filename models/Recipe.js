0609e41d6ad3d8960f92fe4b6d7043441cf33a07
cgorrino <cgorrino03@gmail.com>
2021-06-16 20:38:07 -0700

    Adding Recipe model definition.

---
+++ models/Recipe.js
@@ -0,0 +1,48 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model {}
+
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        recipe_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        recipe_description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        number_servings: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        unit_price: {
+            type: DataTypes.DECIMAL(12,2),
+            allowNull: false,
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'category',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'recipe',
+    }
+);
+
+module.exports = Recipe;
