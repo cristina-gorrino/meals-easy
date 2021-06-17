490577557e5125b99574e5109518ea3286d0f136
cgorrino <cgorrino03@gmail.com>
2021-06-16 21:25:49 -0700

    Adding Order Item model.

---
+++ models/OrderItem.js
@@ -0,0 +1,48 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class OrderItem extends Model {}
+
+OrderItem.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    subtotal_price: {
+        type: DataTypes.DECIMAL(12,2),
+        allowNull: false
+    },
+    shopping_cart_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'shopping_cart',
+            key: 'id',
+        },
+    },
+    recipe_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'recipe',
+            key: 'id'
+        }
+    },
+
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'order_item',
+}
+);
+
+module.exports = OrderItem;
