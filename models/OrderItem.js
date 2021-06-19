2cc6baf500900cc8aec3633c72ef7e91cab579ad
Zahra Seyedi <za.seyedi@gmail.com>
2021-06-19 09:23:19 -0700

    Dependency chain problem: ingredient -> recipe -> order_item => recipe

--- models/OrderItem.js
+++ models/OrderItem.js
@@ -1,48 +1,49 @@
-const {Model, DataTypes} = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class OrderItem extends Model {}
 
-OrderItem.init({
+OrderItem.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     quantity: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     subtotal_price: {
-        type: DataTypes.DECIMAL(12,2),
-        allowNull: false
+      type: DataTypes.DECIMAL(12, 2),
+      allowNull: false,
     },
     shopping_cart_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'shopping_cart',
-            key: 'id',
-        },
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "shopping_cart",
+        key: "id",
+      },
     },
-    recipe_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'recipe',
-            key: 'id'
-        }
-    },
-
-},
-{
+    //Dependency chain: ingredient -> recipe -> order_item => recipe
+    // recipe_id: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   references: {
+    //     model: "recipe",
+    //     key: "id",
+    //   },
+    // },
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'order_item',
-}
+    modelName: "order_item",
+  }
 );
 
 module.exports = OrderItem;
