b07a35d0984bbd6118d3308d2761c5a904dfffa9
Zahra Seyedi <za.seyedi@gmail.com>
2021-06-19 09:15:53 -0700

    exports Category

--- models/category.js
+++ models/category.js
@@ -3,22 +3,26 @@ const sequelize = require("../config/connection");
 
 class Category extends Model {}
 
-Category.init({
-  Category_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  Category_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  sequelize,
-  timestamps: true,
-  freezeTableName: true,
-  underscored: true,
-  modelName: "category",
-});
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
 
-module.exports = User;
+module.exports = Category;
