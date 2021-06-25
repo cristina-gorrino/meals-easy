30544d96af2ea6fef864b8e0047faad9deb0df48
cgorrino <cgorrino03@gmail.com>
2021-06-24 21:13:31 -0700

    Removing unused logger.

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const app = express();
 
 //app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(logger("dev"));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "public")));
